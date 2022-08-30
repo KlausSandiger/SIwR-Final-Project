b2968cf468a02203b58d68c6538c69f90e79082a
KlausSandiger <mikolaj.piaszczynski1999@gmail.com>
2022-08-30 20:12:51 +0200

    Slight improvement to output visibility. Corrected CoordinateConversion function.

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import getopt
 import os.path
 import sys
 import cv2
-
-import frame_operations
+import numpy as np
 
 def CoordinatesConversion(coordinates_as_str):
 
-    print(len(coordinates_as_str))
+    dim = int(len(coordinates_as_str))
     coordinates_as_int = []
-
     for i in range(int(len(coordinates_as_str))):
-        j = int(i)
         x = int(float(coordinates_as_str[i][0]))
         y = int(float(coordinates_as_str[i][1]))
         w = int(float(coordinates_as_str[i][2]))
@@ -21,9 +18,9 @@ def CoordinatesConversion(coordinates_as_str):
         coordinates_as_int.append(w)
         coordinates_as_int.append(h)
 
-    print(coordinates_as_int)
+    Converted2Int = np.reshape(coordinates_as_int,(dim,4))
 
-
+    return Converted2Int
 
 def SetUpFiles(directory_path = "", description_file = "bboxes.txt"):
 
@@ -38,21 +35,12 @@ def SetUpFiles(directory_path = "", description_file = "bboxes.txt"):
     prev_name = 0
     prev_number_of_bb = 0
 
-
-
     while True:
 
         images = []
         objects = []
         number_of_bb = []
         coordinates = []
-
-
-#Apending .jpg and .png files to the array
-#         for file in files:
-#             if file.endswith("jpg") or file.endswith("png"):
-#                 images.append(file)
-
 
         name = file.readline().rstrip("\n")
         if not name:
@@ -74,16 +62,8 @@ def SetUpFiles(directory_path = "", description_file = "bboxes.txt"):
             prev_img = cv2.imread(files + prev_name)
             cv2.imshow("prev", prev_img)
 
-        print("BBoxes current " + number_of_bb)
-        print("BBoxes prev " + str(prev_number_of_bb))
-        gap = int(number_of_bb) - int(prev_number_of_bb)
-
         prev_name = name
         prev_number_of_bb = number_of_bb
-
-        print("Change in between frames " + str(gap))
-
-        print(type(coordinates))
 
         CoordinatesConversion(coordinates)
 
